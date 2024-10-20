80652cd1956110dc6a38029d4d50c1526ddaaa69
Federico Zamora <federicozamora616@gmail.com>
2024-10-19 22:44:04 -0300

    se creo el esquema de la base de datos

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 const express = require("express")
+const dbconnect = require('./config/db')
 
 
 const app = express()
 
 const port = process.env.PORT
 
-app.listen(port,()=>{
-    console.log(`api funcionando`)
+
+dbconnect().then(()=>{
+    app.listen(port,()=>{
+        console.log("api funcionando")
+    })
+}).catch(err =>{
+    console.log(`no se pudo conectar la api por el error = ${err}`)
 })
+
